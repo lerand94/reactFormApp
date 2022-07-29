e9f6154e8f618e8119fba3d7e3b9cd7c2d1603dc
Kirill <27140985+lerand94@users.noreply.github.com>
2022-07-29 16:17:42 +0300

    added 2 approach to reading data from input and reset function

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,35 @@
+import { useRef, useState } from "react";
+
 const SimpleInput = (props) => {
+  const [enteredValue, setEnteredValue] = useState("");
+
+  const enteredRef = useRef();
+
+  const enteredValueHandler = (e) => {
+    setEnteredValue(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    console.log(enteredValue);
+
+    console.log(enteredRef.current.value);
+
+    setEnteredValue("");
+    enteredRef.current.value = ""; // not IDEAL, react in perfect dont need to manipulate DOM
+  };
+
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={submitHandler}>
+      <div className="form-control">
+        <label htmlFor="name">Your Name</label>
+        <input
+          ref={enteredRef}
+          type="text"
+          id="name"
+          onChange={enteredValueHandler}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
